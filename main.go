748314a7efa4a9ecc632c591259dc3d7c93657f5
mel <mcmelon@nodiumhosting.com>
2026-07-22 22:20:55 +0200

    feat: main screen

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gen2brain/raylib-go/raylib"
 	"github.com/threeidiotsonegamejam/gmtk26/src/math"
 	"github.com/threeidiotsonegamejam/gmtk26/src/ui"
@@ -15,36 +17,51 @@ func tick() {
 
 }
 
+var mainScreen = ui.Screen{
+	Elements: []ui.Element{
+		&ui.TextElement{
+			Pos: func(renderWidth, renderHeight int32) math.Vec2i {
+				return math.Vec2i{
+					X: renderWidth / 2,
+					Y: renderHeight / 4,
+				}
+			},
+			Text:      "Game",
+			TextSize:  96,
+			TextColor: rl.Black,
+		},
+		&ui.ButtonElement{
+			Pos: func(renderWidth, renderHeight int32) math.Vec2i {
+				return math.Vec2i{
+					X: renderWidth / 2,
+					Y: renderHeight / 2,
+				}
+			},
+			Text:         "Play",
+			TextSize:     48,
+			Padding:      8,
+			OutlineWidth: 4,
+			Colors: ui.ButtonColors{
+				Outline:         rl.Gray,
+				Background:      rl.LightGray,
+				BackgroundHover: math.ColorAdd(rl.LightGray, 25),
+				BackgroundClick: math.ColorAdd(rl.LightGray, 40),
+				Foreground:      rl.DarkGray,
+			},
+			Click: func() {
+				fmt.Println("clicked")
+			},
+		},
+	},
+}
+
 func frame() {
 	rl.BeginDrawing()
 
 	rl.ClearBackground(rl.RayWhite)
 
-	text, textHeight := "Game", int32(96)
-	textWidth := rl.MeasureText(text, textHeight)
-	midX, midY := int32(rl.GetRenderWidth()/2), int32(rl.GetRenderHeight()/2)
-
-	rl.DrawText(text, midX-(textWidth/2), int32(rl.GetRenderHeight()/4), textHeight, rl.Black)
-
-	btn := ui.ButtonElement{
-		CenterPos: math.Vec2i{
-			X: midX,
-			Y: midY,
-		},
-		Text:         "Play",
-		TextSize:     48,
-		Padding:      8,
-		OutlineWidth: 4,
-		Colors: ui.ButtonColors{
-			Outline:         rl.Gray,
-			Background:      rl.LightGray,
-			BackgroundHover: math.ColorAdd(rl.LightGray, 25),
-			BackgroundClick: math.ColorAdd(rl.LightGray, 40),
-			Foreground:      rl.DarkGray,
-		},
-		Click: func() {},
-	}
-	btn.Draw()
+	mainScreen.Update(0)
+	mainScreen.Draw()
 
 	rl.EndDrawing()
 }
