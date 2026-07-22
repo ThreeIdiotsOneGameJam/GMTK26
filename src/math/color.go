9908830c2ee73be950ad7c14c134690c06d208fd
mel <mcmelon@nodiumhosting.com>
2026-07-22 22:49:22 +0200

    feat: improve button color fallback handling

--- src/math/color.go
+++ src/math/color.go
@@ -2,16 +2,16 @@ package math
 
 import "image/color"
 
-func ColorAdd(color color.RGBA, n uint8) color.RGBA {
+func ColorAdd(color color.RGBA, n uint8) *color.RGBA {
 	color.R = Clampb(int32(color.R) + int32(n))
 	color.G = Clampb(int32(color.G) + int32(n))
 	color.B = Clampb(int32(color.B) + int32(n))
-	return color
+	return &color
 }
 
-func ColorSub(color color.RGBA, n uint8) color.RGBA {
+func ColorSub(color color.RGBA, n uint8) *color.RGBA {
 	color.R = Clampb(int32(color.R) - int32(n))
 	color.G = Clampb(int32(color.G) - int32(n))
 	color.B = Clampb(int32(color.B) - int32(n))
-	return color
+	return &color
 }
--- src/ui/button_element.go
+++ src/ui/button_element.go
@@ -5,7 +5,6 @@ import (
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 	"github.com/threeidiotsonegamejam/gmtk26/src/math"
-	"github.com/threeidiotsonegamejam/gmtk26/src/util"
 )
 
 type ButtonElement struct {
@@ -27,16 +26,16 @@ type ButtonElement struct {
 
 type ButtonColors struct {
 	Outline      color.RGBA
-	OutlineHover color.RGBA
-	OutlineClick color.RGBA
+	OutlineHover *color.RGBA
+	OutlineClick *color.RGBA
 
 	Background      color.RGBA
-	BackgroundHover color.RGBA
-	BackgroundClick color.RGBA
+	BackgroundHover *color.RGBA
+	BackgroundClick *color.RGBA
 
 	Foreground      color.RGBA
-	ForegroundHover color.RGBA
-	ForegroundClick color.RGBA
+	ForegroundHover *color.RGBA
+	ForegroundClick *color.RGBA
 }
 
 func (b *ButtonElement) update(delta float32) {
@@ -58,29 +57,36 @@ func (b *ButtonElement) draw() {
 	btnWidthOuter, btnHeightOuter := b.w+b.OutlineWidth*2, b.h+b.OutlineWidth*2
 	btnStartXOuter, btnStartYOuter := b.x-b.OutlineWidth, b.y-b.OutlineWidth
 
-	oCol := b.Colors.Outline
-	bgCol := b.Colors.Background
-	fgCol := b.Colors.Foreground
+	oCol := &b.Colors.Outline
+	bgCol := &b.Colors.Background
+	fgCol := &b.Colors.Foreground
 
 	if b.hovered {
 		rl.SetMouseCursor(rl.MouseCursorPointingHand)
 
-		oCol = util.IfElse(b.Colors.OutlineHover != color.RGBA{}, b.Colors.OutlineHover, oCol)
-		bgCol = util.IfElse(b.Colors.BackgroundHover != color.RGBA{}, b.Colors.BackgroundHover, bgCol)
-		fgCol = util.IfElse(b.Colors.ForegroundHover != color.RGBA{}, b.Colors.ForegroundHover, fgCol)
+		oCol = fallbackColor(b.Colors.OutlineHover, oCol)
+		bgCol = fallbackColor(b.Colors.BackgroundHover, bgCol)
+		fgCol = fallbackColor(b.Colors.ForegroundHover, fgCol)
 
 		if rl.IsMouseButtonDown(rl.MouseButtonLeft) {
-			oCol = util.IfElse(b.Colors.OutlineClick != color.RGBA{}, b.Colors.OutlineClick, oCol)
-			bgCol = util.IfElse(b.Colors.BackgroundClick != color.RGBA{}, b.Colors.BackgroundClick, bgCol)
-			fgCol = util.IfElse(b.Colors.ForegroundClick != color.RGBA{}, b.Colors.ForegroundClick, fgCol)
+			oCol = fallbackColor(b.Colors.OutlineClick, oCol)
+			bgCol = fallbackColor(b.Colors.BackgroundClick, bgCol)
+			fgCol = fallbackColor(b.Colors.ForegroundClick, fgCol)
 		}
 	} else {
 		rl.SetMouseCursor(rl.MouseCursorDefault)
 	}
 
-	rl.DrawRectangle(btnStartXOuter, btnStartYOuter, btnWidthOuter, btnHeightOuter, oCol)
+	rl.DrawRectangle(btnStartXOuter, btnStartYOuter, btnWidthOuter, btnHeightOuter, *oCol)
 
-	rl.DrawRectangle(b.x, b.y, b.w, b.h, bgCol)
+	rl.DrawRectangle(b.x, b.y, b.w, b.h, *bgCol)
 
-	rl.DrawText(b.Text, b.cx-b.textWidth/2, b.cy-b.TextSize/2, b.TextSize, fgCol)
+	rl.DrawText(b.Text, b.cx-b.textWidth/2, b.cy-b.TextSize/2, b.TextSize, *fgCol)
+}
+
+func fallbackColor(override, fallback *color.RGBA) *color.RGBA {
+	if override != nil {
+		return override
+	}
+	return fallback
 }
