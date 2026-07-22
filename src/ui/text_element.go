1ca48110eacdc65ff8eb56ddc8faf7649689e044
mel <mcmelon@nodiumhosting.com>
2026-07-22 22:19:37 +0200

    feat: text element

---
+++ src/ui/text_element.go
@@ -0,0 +1,28 @@
+package ui
+
+import (
+	"image/color"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"github.com/threeidiotsonegamejam/gmtk26/src/math"
+)
+
+type TextElement struct {
+	Pos       func(renderWidth, renderHeight int32) math.Vec2i
+	Text      string
+	TextSize  int32
+	TextColor color.RGBA
+
+	x, y, w int32
+}
+
+func (e *TextElement) update(delta float32) {
+	e.w = rl.MeasureText(e.Text, e.TextSize)
+
+	pos := e.Pos(int32(rl.GetRenderWidth()), int32(rl.GetRenderHeight()))
+	e.x, e.y = pos.X-e.w/2, pos.Y-e.TextSize/2
+}
+
+func (e *TextElement) draw() {
+	rl.DrawText(e.Text, e.x, e.y, e.TextSize, e.TextColor)
+}
