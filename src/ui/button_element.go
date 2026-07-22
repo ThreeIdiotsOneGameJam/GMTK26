94ebb9cbdad4c030347d2a39270628d67699e92c
mel <mcmelon@nodiumhosting.com>
2026-07-22 22:20:37 +0200

    refactor: clean up button element

--- src/ui/button_element.go
+++ src/ui/button_element.go
@@ -5,17 +5,24 @@ import (
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 	"github.com/threeidiotsonegamejam/gmtk26/src/math"
+	"github.com/threeidiotsonegamejam/gmtk26/src/util"
 )
 
 type ButtonElement struct {
-	// CenterPos and Size do not account for the outline, which is rendered outside this
+	// Pos and Size do not account for the outline, which is rendered outside this
 	// If Size is 0 it will be calculated from TextSize
-	CenterPos, Size       math.Vec2i
+	Pos                   func(renderWidth, renderHeight int32) math.Vec2i
+	Size                  math.Vec2i
 	Text                  string
 	TextSize              int32
 	Padding, OutlineWidth int32
 	Colors                ButtonColors
 	Click                 func()
+
+	x, y, cx, cy, w, h, textWidth int32
+
+	hovered, hoveredPrevious bool
+	clicked, clickedPrevious bool
 }
 
 type ButtonColors struct {
@@ -32,37 +39,40 @@ type ButtonColors struct {
 	ForegroundClick color.RGBA
 }
 
-func (b *ButtonElement) Update(delta float32) {}
-func (b *ButtonElement) Draw() {
-	textWidth := rl.MeasureText(b.Text, b.TextSize)
+func (b *ButtonElement) update(delta float32) {
+	b.textWidth = rl.MeasureText(b.Text, b.TextSize)
 
-	btnWidthInner, btnHeightInner := textWidth+b.Padding*2, b.TextSize+b.Padding*2
-	w, h := math.Maxi(btnWidthInner, b.Size.X), math.Maxi(btnHeightInner, b.Size.Y)
-	btnStartXInner, btnStartYInner := b.CenterPos.X-w/2, b.CenterPos.Y-h/2
+	pos := b.Pos(int32(rl.GetRenderWidth()), int32(rl.GetRenderHeight()))
+	b.x, b.y, b.cx, b.cy = pos.X-b.w/2, pos.Y-b.h/2, pos.X, pos.Y
 
-	btnWidthOuter, btnHeightOuter := w+b.OutlineWidth*2, h+b.OutlineWidth*2
-	btnStartXOuter, btnStartYOuter := b.CenterPos.X-w/2-b.OutlineWidth, b.CenterPos.Y-h/2-b.OutlineWidth
+	b.w, b.h = math.Maxi(b.textWidth+b.Padding*2, b.Size.X), math.Maxi(b.TextSize+b.Padding*2, b.Size.Y)
 
-	btnHovered := rl.GetMouseX() > btnStartXInner &&
-		rl.GetMouseX() < btnStartXInner+w &&
-		rl.GetMouseY() > btnStartYInner &&
-		rl.GetMouseY() < btnStartYInner+h
+	mouseX, mouseY := rl.GetMouseX(), rl.GetMouseY()
+	b.hovered = mouseX > b.x &&
+		mouseX < b.x+b.w &&
+		mouseY > b.y &&
+		mouseY < b.y+b.h
+}
+
+func (b *ButtonElement) draw() {
+	btnWidthOuter, btnHeightOuter := b.w+b.OutlineWidth*2, b.h+b.OutlineWidth*2
+	btnStartXOuter, btnStartYOuter := b.x-b.OutlineWidth, b.y-b.OutlineWidth
 
 	oCol := b.Colors.Outline
 	bgCol := b.Colors.Background
 	fgCol := b.Colors.Foreground
 
-	if btnHovered {
+	if b.hovered {
 		rl.SetMouseCursor(rl.MouseCursorPointingHand)
 
-		oCol = b.Colors.OutlineHover
-		bgCol = b.Colors.BackgroundHover
-		fgCol = b.Colors.ForegroundHover
+		oCol = util.IfElse(b.Colors.OutlineHover != color.RGBA{}, b.Colors.OutlineHover, oCol)
+		bgCol = util.IfElse(b.Colors.BackgroundHover != color.RGBA{}, b.Colors.BackgroundHover, bgCol)
+		fgCol = util.IfElse(b.Colors.ForegroundHover != color.RGBA{}, b.Colors.ForegroundHover, fgCol)
 
 		if rl.IsMouseButtonDown(rl.MouseButtonLeft) {
-			oCol = b.Colors.OutlineClick
-			bgCol = b.Colors.BackgroundClick
-			fgCol = b.Colors.ForegroundClick
+			oCol = util.IfElse(b.Colors.OutlineClick != color.RGBA{}, b.Colors.OutlineClick, oCol)
+			bgCol = util.IfElse(b.Colors.BackgroundClick != color.RGBA{}, b.Colors.BackgroundClick, bgCol)
+			fgCol = util.IfElse(b.Colors.ForegroundClick != color.RGBA{}, b.Colors.ForegroundClick, fgCol)
 		}
 	} else {
 		rl.SetMouseCursor(rl.MouseCursorDefault)
@@ -70,7 +80,7 @@ func (b *ButtonElement) Draw() {
 
 	rl.DrawRectangle(btnStartXOuter, btnStartYOuter, btnWidthOuter, btnHeightOuter, oCol)
 
-	rl.DrawRectangle(btnStartXInner, btnStartYInner, w, h, bgCol)
+	rl.DrawRectangle(b.x, b.y, b.w, b.h, bgCol)
 
-	rl.DrawText(b.Text, b.CenterPos.X-textWidth/2, b.CenterPos.Y-b.TextSize/2, b.TextSize, fgCol)
+	rl.DrawText(b.Text, b.cx-b.textWidth/2, b.cy-b.TextSize/2, b.TextSize, fgCol)
 }
