895efefe065a7a92711c35566412286f4c600d4d
mel <mcmelon@nodiumhosting.com>
2026-07-22 21:11:37 +0200

    refactor: remove pos and size getters from element

--- src/ui/ui.go
+++ src/ui/ui.go
@@ -1,13 +1,6 @@
 package ui
 
-import (
-	"github.com/threeidiotsonegamejam/gmtk26/src/math"
-)
-
 type Element interface {
-	GetPos() math.Vec2i
-	GetSize() math.Vec2i
-
 	Update(delta float32)
 	Draw()
 }
