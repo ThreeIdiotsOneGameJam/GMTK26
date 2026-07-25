ee52384a266bf4beecefc2aaef73032bec417f10
mel <mcmelon@nodiumhosting.com>
2026-07-25 15:42:15 +0200

    refactor: remove unnecessary colorset constructors

--- src/ui/color.go
+++ src/ui/color.go
@@ -20,50 +20,6 @@ type ColorSet struct {
 	Disabled *color.RGBA
 }
 
-func NewColorSet(defaultColor *color.RGBA) ColorSet {
-	if defaultColor == nil {
-		panic("ColorSet.Default must not be nil")
-	}
-
-	return ColorSet{
-		Default: defaultColor,
-	}
-}
-
-func NewColorSetHover(defaultColor, hoverColor *color.RGBA) ColorSet {
-	if defaultColor == nil {
-		panic("ColorSet.Default must not be nil")
-	}
-
-	return ColorSet{
-		Default: defaultColor,
-		Hover:   hoverColor,
-	}
-}
-
-func NewColorSetClick(defaultColor, clickColor *color.RGBA) ColorSet {
-	if defaultColor == nil {
-		panic("ColorSet.Default must not be nil")
-	}
-
-	return ColorSet{
-		Default: defaultColor,
-		Click:   clickColor,
-	}
-}
-
-func NewColorSetHoverClick(defaultColor, hoverColor, clickColor *color.RGBA) ColorSet {
-	if defaultColor == nil {
-		panic("ColorSet.Default must not be nil")
-	}
-
-	return ColorSet{
-		Default: defaultColor,
-		Hover:   hoverColor,
-		Click:   clickColor,
-	}
-}
-
 func (cs ColorSet) Color(state UIState) *color.RGBA {
 	if cs.Default == nil {
 		panic("ColorSet.Default must not be nil")
